ca6c460f2cc3cccf9a22f9d8039d7c417491d154
matthc <matthieucesbron@gmail.com>
2016-09-30 12:17:34 +0200

    titre formulaire bonjour visiteur

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('have fun !');
+$("button").click(function(){
+
+	var obj = {
+		nom: $('#last_name').val(),
+		prenom: $('#first_name').val(),
+		city: $('#city').val(),
+	}
+	console.log(obj);
+	$("#username").text(obj.nom + " " + obj.prenom);
+});
+
+
